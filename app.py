6f339ee0e90e7c4f14f36da1c0fbfb0c8f19296b
naim.assoum <naim.assoum@gmail.com>
2022-05-13 09:28:33 +0000

    push_final

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
 
 @app.route('/hello/<username>')
 def hello_user(username):
-    return 'Salut %s! :) \n' % username
+    return 'wesh %s! :) \n' % username
 
 @app.route("/contact")
 def page_contact():
@@ -26,4 +26,4 @@ def page_a_propos():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port='5100',debug=True)
