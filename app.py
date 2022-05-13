1e5a477bd3309634c8ec46454337b6007bd6bd09
naim.assoum <naim.assoum@gmail.com>
2022-05-13 11:20:49 +0000

    push_final

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     return render_template("index.html")
 
 def hello():
-    return 'Helloooo World!\n'
+    return 'Hellooooo World!\n'
 
 @app.route('/hello/<username>')
 def hello_user(username):
